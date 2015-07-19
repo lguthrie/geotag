e09ec6386a84e65a71a3ba875dfff26c33e7b216
Loren Guthrie <lguthrie@gmail.com>
2015-07-18 20:06:44 -0700

    We  can add, remove, and list messages. Next we need to filter the messages by location and time.

--- server.py
+++ server.py
@@ -2,35 +2,53 @@
 """server.py
 
 This is the request handler that implements the endpoints"""
-from flask import Flask, abort
+import json
+import uuid
+from flask import Flask, abort, request
+import time
 
+MESSAGES = []  # We'll just use a global list until we run out of memory
 app = Flask(__name__)
 
 
-def add_message():
-    """Add a message to the system"""
-    raise NotImplemented
+def add_or_update_message():
+    """Add a message to the system or update it if a message id is specified"""
+    message = request.get_json()
+    # TODO: Check to see if we got a message_id in the request, if so update message
+    timestamp = time.time()
+    message_id = str(uuid.uuid4())
+    message['message_id'] = message_id
+    message['timestamp'] = timestamp
+    MESSAGES.append(message)
+    response = {'message_id': message_id}
+    return json.dumps(response)
 
 
 def get_messages():
     """Get all the messages nearby"""
-    raise NotImplemented
+    # TODO: Filter by location
+    return json.dumps(MESSAGES)
 
 
 def remove_message():
     """Remove a message"""
-    raise NotImplemented
+    data = request.get_json()
+    message_id = data['message_id']
+    for idx, message in enumerate(MESSAGES):
+        if message['message_id'] == message_id:
+            MESSAGES.pop(idx)
+            return ''
+    abort(400)
 
 
-# noinspection PyUnresolvedReferences
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST', 'DELETE'])
 def handle_request():
     if request.method == 'POST':
-        add_message()
+        return add_or_update_message()
     elif request.method == 'GET':
-        get_messages()
+        return get_messages()
     elif request.method == 'DELETE':
-        remove_message()
+        return remove_message()
     else:
         abort(400)
 
